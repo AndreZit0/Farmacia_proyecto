892c0b3ee6fa893f23a32261108bb56c0078749f
alojoarboleda2003 <alonsoarboleda17@gmail.com>
2025-03-10 11:07:21 -0500

    primer push

--- src/ClienteChat.java
+++ src/ClienteChat.java
@@ -1,4 +1,12 @@
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class ClienteChat {
+
     public static String serverAdress = "";
     public static String sendMessage = "";
     public void clienteConexion(){
